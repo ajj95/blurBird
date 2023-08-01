92b7fc0cb3e15e6ebbbfff331ffed36c16d3d781
maverick <doncic1990@gmail.com>
2023-08-01 18:18:08 +0900

    Feat: Structure about Info

--- src/main/java/org/blurbird/controller/InfoController.java
+++ src/main/java/org/blurbird/controller/InfoController.java
@@ -1,6 +1,14 @@
 package org.blurbird.controller;
 
+import java.util.List;
+
+import org.blurbird.domain.bank.BusinessVO;
+import org.blurbird.service.info.InfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,10 +20,15 @@ import lombok.extern.log4j.Log4j;
 @RequestMapping("/info")
 @AllArgsConstructor
 public class InfoController {
+	
+	@Autowired
+	private InfoService service;
 
 	@GetMapping("/infoTA")
-	public void getInfoTA() {
-		
+	public void getInfoTA(Model model) {
+		log.info(service.getListBusiness());
+		model.addAttribute("list", service.getListBusiness());
+		model.addAttribute("sales", service.getTotalSales());
 	}
 	
 	@GetMapping("/infoCO")
--- src/main/java/org/blurbird/service/info/InfoServiceImpl.java
+++ src/main/java/org/blurbird/service/info/InfoServiceImpl.java
@@ -1,5 +1,41 @@
 package org.blurbird.service.info;
 
+import java.util.List;
+
+import org.blurbird.domain.bank.BusinessVO;
+import org.blurbird.domain.info.IncomeSVO;
+import org.blurbird.mapper.InfoMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
+@Service
+@Log4j
+@AllArgsConstructor
 public class InfoServiceImpl implements InfoService {
+	
+	@Autowired
+	private InfoMapper mapper;
+
+	@Override
+	public List<BusinessVO> getListBusiness() {
+			log.info("수임사 목록 가져오기");
+		return mapper.getListBusiness();
+	}
+
+	@Override
+	public IncomeSVO getTotalSales() {
+			log.info("총 매출 찍어내기");
+			IncomeSVO vo = new IncomeSVO();
+			vo.setSalesfromautoslip(mapper.getSalesfromAuto().getSalesfromautoslip());
+			vo.setSalesfrombankslip(mapper.getSalesfromBank().getSalesfrombankslip());
+			vo.setTotalsales(mapper.getSalesfromAuto().getSalesfromautoslip() + mapper.getSalesfromBank().getSalesfrombankslip());
+			
+		return vo;
+	}
+	
+	
 
 }
