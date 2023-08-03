5244f992d03d027e1ecef6d569279611655edaff
maverick <doncic1990@gmail.com>
2023-08-03 18:09:09 +0900

    Feat: modify method for info

--- src/main/java/org/blurbird/service/info/InfoService.java
+++ src/main/java/org/blurbird/service/info/InfoService.java
@@ -10,9 +10,7 @@ public interface InfoService {
 
 	public List<BusinessVO> getListBusiness();
 	
-//	public List<IncomeSVO> getISElements(Long bizno, String year);
-	
-	public Long getSalesfromAuto(InfoData infoData);
+	public IncomeSVO getISByBiz(InfoData infoData);
 	
 	public List<InfoData> getList(InfoData infoData);
 }
--- src/main/java/org/blurbird/service/info/InfoServiceImpl.java
+++ src/main/java/org/blurbird/service/info/InfoServiceImpl.java
@@ -18,118 +18,125 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 @AllArgsConstructor
 public class InfoServiceImpl implements InfoService {
-	
+
 	@Autowired
 	private InfoMapper mapper;
-	
+
 	@Override
 	public List<BusinessVO> getListBusiness() {
-			log.info("수임사 목록 가져오기");
+		log.info("수임사 목록 가져오기");
 		return mapper.getListBusiness();
 	}
 
-	/*
-	 * @Override public List<IncomeSVO> getISElements(Long bizno, String year) {
-	 * 
-	 * log.info(year);
-	 * 
-	 * IncomeSVO vo = new IncomeSVO(); List<IncomeSVO> voList = new
-	 * ArrayList<IncomeSVO>();
-	 * 
-	 * vo.setSalesfromautoslip(mapper.getSalesfromAuto(bizno, year));
-	 * vo.setSalesfrombankslip(mapper.getSalesfromBank(bizno, year));
-	 * 
-	 * long totalSales = mapper.getSalesfromAuto(bizno, year) +
-	 * mapper.getSalesfromBank(bizno, year); log.info("Sales: " + totalSales);
-	 * 
-	 * vo.setTotalsales(totalSales);
-	 * 
-	 * 
-	 * vo.setCogsfromautoslip(mapper.getCOGSfromAuto(bizno, year));
-	 * vo.setCogsfrombankslip(mapper.getCOGSfromBank(bizno, year));
-	 * vo.setCogsfromcashslip(mapper.getCOGSfromCash(bizno, year));
-	 * 
-	 * long totalCOGS = mapper.getCOGSfromAuto(bizno, year) +
-	 * mapper.getCOGSfromBank(bizno, year) + mapper.getCOGSfromCash(bizno, year);
-	 * log.info("COGS: " + totalCOGS);
-	 * 
-	 * vo.setTotalcogs(totalCOGS);
-	 * 
-	 * 
-	 * 
-	 * vo.setOperExpensefromAutoSlip(mapper.getOperExpensefromAuto(bizno, year));
-	 * vo.setOperExpensefromBankSlip(mapper.getOperExpensefromBank(bizno, year));
-	 * vo.setOperExpensefromCashSlip(mapper.getOperExpensefromCash(bizno, year));
-	 * 
-	 * long totalOperExpense = mapper.getOperExpensefromAuto(bizno, year) +
-	 * mapper.getOperExpensefromBank(bizno, year) +
-	 * mapper.getOperExpensefromCash(bizno, year);
-	 * 
-	 * log.info("OE: " + totalOperExpense);
-	 * 
-	 * vo.setTotaloe(totalOperExpense);
-	 * 
-	 * 
-	 * vo.setBusinessIncome(totalSales - (totalCOGS + totalOperExpense));
-	 * 
-	 * log.info("======================================="); log.info("사업소득금액: " +
-	 * vo.getBusinessIncome());
-	 * 
-	 * 
-	 * double incomeBeforeTax = vo.getBusinessIncome().doubleValue();
-	 * 
-	 * if(incomeBeforeTax<=12000000) { vo.setTax((long)(incomeBeforeTax*0.06)); }
-	 * else if(12000000< incomeBeforeTax && incomeBeforeTax <= 46000000) {
-	 * vo.setTax((long)((incomeBeforeTax*0.15)-1080000)); } else
-	 * if(46000000<incomeBeforeTax && incomeBeforeTax <= 88000000) {
-	 * vo.setTax((long)((incomeBeforeTax*0.24)-5220000)); }else
-	 * if(88000000<incomeBeforeTax && incomeBeforeTax <= 150000000) {
-	 * vo.setTax((long)((incomeBeforeTax*0.35)-14900000)); }else
-	 * if(150000000<incomeBeforeTax && incomeBeforeTax <= 300000000) {
-	 * vo.setTax((long)((incomeBeforeTax*0.38)-19400000)); }else
-	 * if(300000000<incomeBeforeTax && incomeBeforeTax <= 500000000) {
-	 * vo.setTax((long)((incomeBeforeTax*0.40)-25400000)); }else
-	 * if(500000000<incomeBeforeTax && incomeBeforeTax <= 1000000000) {
-	 * vo.setTax((long)((incomeBeforeTax*0.42)-35400000)); }else {
-	 * vo.setTax((long)((incomeBeforeTax*0.45)-65400000)); }
-	 * 
-	 * voList.add(vo);
-	 * 
-	 * return voList; }
-	 */
-
 	@Override
-	public Long getSalesfromAuto(InfoData infoData) {
-		log.info(infoData.getYear().toString());
-		return mapper.getSalesfromAuto(infoData);
+	public IncomeSVO getISByBiz(InfoData infoData) {
+
+		IncomeSVO iS = new IncomeSVO();
+
+		iS.setBizno(infoData.getBizno());
+
+		if (mapper.getSalesfromAuto(infoData) == null) {
+			// null일 경우에 0으로 설정
+			iS.setSalesfromautoslip(0L);
+		} else {
+			iS.setSalesfromautoslip(mapper.getSalesfromAuto(infoData));
+		}
+
+		if (mapper.getSalesfromBank(infoData) == null) {
+			// null일 경우에 0으로 설정
+			iS.setSalesfrombankslip(0L);
+		} else {
+			iS.setSalesfrombankslip(mapper.getSalesfromBank(infoData));
+		}
+
+		iS.setTotalsales(iS.getSalesfromautoslip() + iS.getSalesfrombankslip());
+
+		if (mapper.getCOGSfromAuto(infoData) == null) {
+			// null일 경우에 0으로 설정
+			iS.setCogsfromautoslip(0L);
+		} else {
+			iS.setCogsfromautoslip(mapper.getCOGSfromAuto(infoData));
+		}
+		if (mapper.getCOGSfromBank(infoData) == null) {
+			// null일 경우에 0으로 설정
+			iS.setCogsfrombankslip(0L);
+		} else {
+			iS.setCogsfrombankslip(mapper.getCOGSfromBank(infoData));
+		}
+		if (mapper.getCOGSfromCash(infoData) == null) {
+			// null일 경우에 0으로 설정
+			iS.setCogsfromcashslip(0L);
+		} else {
+			iS.setCogsfromcashslip(mapper.getCOGSfromCash(infoData));
+		}
+
+		iS.setTotalcogs(iS.getCogsfromautoslip() + iS.getCogsfrombankslip() + iS.getCogsfromcashslip());
+
+		if (mapper.getOperExpensefromAuto(infoData) == null) {
+			// null일 경우에 0으로 설정
+			iS.setOperExpensefromAutoSlip(0L);
+		} else {
+			iS.setOperExpensefromAutoSlip(mapper.getOperExpensefromAuto(infoData));
+		}
+		if (mapper.getOperExpensefromBank(infoData) == null) {
+			// null일 경우에 0으로 설정
+			iS.setOperExpensefromBankSlip(0L);
+		} else {
+			iS.setOperExpensefromBankSlip(mapper.getOperExpensefromBank(infoData));
+		}
+		if (mapper.getOperExpensefromCash(infoData) == null) {
+			// null일 경우에 0으로 설정
+			iS.setOperExpensefromCashSlip(0L);
+		} else {
+			iS.setOperExpensefromCashSlip(mapper.getOperExpensefromCash(infoData));
+		}
+
+		iS.setTotaloe(
+				iS.getOperExpensefromAutoSlip() + iS.getOperExpensefromBankSlip() + iS.getOperExpensefromCashSlip());
+
+		iS.setBusinessIncome(iS.getTotalsales() - (iS.getTotalcogs() + iS.getTotaloe()));
+
+		double incomeBeforeTax = iS.getBusinessIncome().doubleValue();
+		if (incomeBeforeTax <= 0) {
+			incomeBeforeTax = 0;
+		}
+
+		if (incomeBeforeTax <= 12000000) {
+			iS.setTax((long) (incomeBeforeTax * 0.06));
+		} else if (12000000 < incomeBeforeTax && incomeBeforeTax <= 46000000) {
+			iS.setTax((long) ((incomeBeforeTax * 0.15) - 1080000));
+		} else if (46000000 < incomeBeforeTax && incomeBeforeTax <= 88000000) {
+			iS.setTax((long) ((incomeBeforeTax * 0.24) - 5220000));
+		} else if (88000000 < incomeBeforeTax && incomeBeforeTax <= 150000000) {
+			iS.setTax((long) ((incomeBeforeTax * 0.35) - 14900000));
+		} else if (150000000 < incomeBeforeTax && incomeBeforeTax <= 300000000) {
+			iS.setTax((long) ((incomeBeforeTax * 0.38) - 19400000));
+		} else if (300000000 < incomeBeforeTax && incomeBeforeTax <= 500000000) {
+			iS.setTax((long) ((incomeBeforeTax * 0.40) - 25400000));
+		} else if (500000000 < incomeBeforeTax && incomeBeforeTax <= 1000000000) {
+			iS.setTax((long) ((incomeBeforeTax * 0.42) - 35400000));
+		} else {
+			iS.setTax((long) ((incomeBeforeTax * 0.45) - 65400000));
+		}
+
+		return iS;
+
 	}
 
 	@Override
 	public List<InfoData> getList(InfoData infoData) {
+		IncomeSVO iS = this.getISByBiz(infoData);
 		List<InfoData> infoList = new ArrayList<InfoData>();
-		List<BusinessVO> bizList = mapper.getListBusiness();
-		
-		
-		for(int i=0; i<bizList.size(); i++) {
-			InfoData info = new InfoData();
-			info.setBizno(mapper.getListBusiness().get(i).getBizno());
-			info.setYear(infoData.getYear());
-			info.setBizname(mapper.getListBusiness().get(i).getBizname());
-			
-			if (mapper.getSalesfromAuto(info) == null) {
-			    // null일 경우에 0으로 설정
-				info.setBizincome("0");
-			} else {
-				info.setBizincome(Long.toString(mapper.getSalesfromAuto(info)));
+
+			if (infoData.getBizno().equals(iS.getBizno())) {
+				infoData.setBizname(mapper.getBizName(infoData).getBizname());
+				infoData.setBizincome(Long.toString(iS.getBusinessIncome()));
+				infoData.setTax(Long.toString(iS.getTax()));
+
+				infoList.add(infoData);
 			}
-			
-			infoList.add(info);
-		}
-		
-		
+
 		return infoList;
 	}
-
-	
 
 }
