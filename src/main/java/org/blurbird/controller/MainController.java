9b879edeb912a8e52bb680d8d31552a5cffe7f33
jj <wkdlwps21@naver.com>
2023-08-04 09:04:55 +0900

    main create ajj 0804

---
+++ src/main/java/org/blurbird/controller/MainController.java
@@ -0,0 +1,16 @@
+package org.blurbird.controller;
+
+import org.blurbird.service.docrequest.DocrequestService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@Log4j
+@RequestMapping("/main/*")
+@AllArgsConstructor
+public class MainController {
+
+}
